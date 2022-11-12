7a73599db8169840075a13fa5a47419f902a5cf3
MajorX234 <majorx234@googlemail.com>
2022-11-12 23:26:39 +0100

    Add BinTree generator and print function

--- src/tree.rs
+++ src/tree.rs
@@ -3,3 +3,46 @@ pub enum BinTree {
     Branch(i64, Box<BinTree>, Box<BinTree>),
     Leaf(i64),
 }
+
+// second implementation
+#[derive(Debug, Default)]
+struct Node<T> {
+    item: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+}
+
+impl BinTree {
+    pub fn generate_tree(depth: i64) -> Self {
+        if depth == 0 {
+            BinTree::Leaf(depth)
+        } else {
+            let left = BinTree::generate_tree(depth - 1);
+            let right = BinTree::generate_tree(depth - 1);
+            BinTree::Branch(depth, Box::new(left), Box::new(right))
+        }
+    }
+
+    fn print_tree_level(&self, level: u32) {
+        match self {
+            BinTree::Branch(item, left_tree, right_tree) => {
+                left_tree.print_tree_level(level + 1);
+                for _ in 0..level {
+                    print!("  ");
+                }
+                println!("B{}", item);
+                right_tree.print_tree_level(level + 1);
+            }
+            BinTree::Leaf(item) => {
+                for _ in 0..level {
+                    print!("  ");
+                }
+                println!("L{}", item);
+            }
+        }
+    }
+
+    pub fn print_tree(&self) {
+        self.print_tree_level(0);
+    }
+}
--- src/tree_print/bin/main.rs
+++ src/tree_print/bin/main.rs
@@ -1,6 +1,6 @@
 use tree_lib::tree::BinTree;
 
 fn main() {
-    let mytree = BinTree::Leaf(1);
-    println!("{:?}", mytree);
+    let mytree = BinTree::generate_tree(5);
+    mytree.print_tree();
 }
